6f498bff39a0765625aa8d96b1fad1f5b09ebf4a
formMeta <formmeta.com@gmail.com>
2023-02-15 12:59:46 +0530

    Add files via upload

---
+++ background.js
@@ -0,0 +1,154 @@
+(() => {
+  var main_array = [];
+  var text_input_count = 0;
+  var input_name_array = [];
+  var text_input_name = [];
+  var name_id = [];
+  var question_array = [];
+
+
+
+
+  const form = document.getElementsByTagName("form")[0];
+  main_array.push(form.action);
+  var B = form.getElementsByClassName("Qr7Oae");
+
+
+
+
+
+  // Getting input value for text inputs
+  var temp = form.querySelectorAll(' div[jsname="o6bZLc"] input');
+  for (let input of temp) {
+    text_input_name.push(input.name);
+  }
+
+
+
+
+
+  // checking whether need to collect email ar not
+  if (
+    form.querySelector('.o3Dpx div[role="listitem"] .M7eMe').innerHTML ===
+    "Email"
+  ) {
+    question_array.push("email", "Email");
+    name_id.push("emailAddress");
+    main_array.push(question_array);
+    question_array = [];
+  }
+
+
+
+
+
+  //  extracting question and input name
+  for (var i = 0; i < B.length; i++) {
+    question_array = [];
+
+
+
+
+
+
+    // for text input
+    if (B[i].getElementsByTagName("input").length>0 && B[i].getElementsByTagName("input")[0].type == "text") {
+      question_array=[];
+      question_array.push("text");
+      question_array.push(B[i].getElementsByClassName("M7eMe")[0].innerHTML);
+      main_array.push(question_array);
+      question_array = [];
+      name_id.push(text_input_name[text_input_count]);
+      text_input_count += 1;
+    }
+
+
+
+
+
+    // for multiple choice input
+    else if (B[i].getElementsByClassName("lLfZXe fnxRtf cNDBpf").length > 0) {
+      if (
+        B[i].getElementsByClassName("lLfZXe fnxRtf cNDBpf")[0].role ==
+        "radiogroup"
+      ) {
+        question_array.push("radio");
+        question_array.push(B[i].getElementsByClassName("M7eMe")[0].innerHTML);
+        var options = B[i].getElementsByClassName(
+          "aDTYNe snByac OvPDhc OIC90c"
+        );
+        // collecting options value
+        var options_array = [];
+        for (var j = 0; j < options.length; j++) {
+          options_array.push(options[j].innerHTML);
+        }
+        question_array.push(options_array);
+        main_array.push(question_array);
+        options_array = [];
+        question_array = [];
+        // getting name value
+        name_id.push(B[i].getElementsByTagName("input")[0].name.replace("_sentinel", ""));
+      }
+    }
+
+
+
+
+    // check box
+    else if (
+      B[i].getElementsByClassName("aDTYNe snByac n5vBHf OIC90c").length > 0
+    ) {
+      question_array = [];
+      question_array.push("checkbox");
+      question_array.push(B[i].getElementsByClassName("M7eMe")[0].innerHTML);
+      var checkValues_array = B[i].getElementsByClassName("aDTYNe snByac n5vBHf OIC90c");
+      var checkValues = [];
+      for (var j = 0; j < checkValues_array.length; j++) {
+        checkValues.push(checkValues_array[j].innerHTML);
+      }
+      question_array.push(checkValues);
+      main_array.push(question_array);
+      question_array = [];
+      checkValues = [];
+      // getting name value
+      name_id.push(
+        (B[i].getElementsByTagName("input")[0].name.replace("_sentinel", ""))
+      );
+    }
+    
+    
+    
+    // for  long paragraph
+    else if (B[i].querySelectorAll("textarea").length > 0) {
+      question_array=[];
+      question_array.push("textarea");
+      question_array.push(B[i].getElementsByClassName("M7eMe")[0].innerHTML);
+      main_array.push(question_array);
+      question_array = [];
+      name_id.push(text_input_name[text_input_count]);
+      text_input_count += 1;
+    }
+
+
+
+    // dropdown 
+    else if(B[i].querySelectorAll('.vRMGwf.oJeWuf').length>0){
+      question_array=[];
+      question_array.push("select");
+      question_array.push(B[i].getElementsByClassName("M7eMe")[0].innerHTML);
+      const elements = B[i].querySelectorAll('.MocG8c[data-value]');
+      const optionsOfDropDown = [];
+      for (let i = 1; i < elements.length; i++) {
+        optionsOfDropDown.push(elements[i].getAttribute('data-value'));
+      }
+      question_array.push(optionsOfDropDown);
+      main_array.push(question_array);
+      question_array=[];
+      name_id.push(text_input_name[text_input_count]);
+      text_input_count += 1;
+    }
+
+  }
+  // return [main_array,name_id];
+  return [main_array,name_id];
+})();
---
+++ popup.js
@@ -0,0 +1,111 @@
+document.addEventListener('DOMContentLoaded', () => {
+   const toggleBtn = document.getElementById('toggle-btn');
+    const inputContainer = document.getElementById('input-container');
+    const inputField = document.getElementById('tab-question');
+
+    toggleBtn.addEventListener('change', () => {
+      if (toggleBtn.checked) {
+        inputContainer.style.display = 'block';
+      } else {
+        inputContainer.style.display = 'none';
+        inputField.value = '';
+      }
+    });
+
+  const button = document.getElementById('button');
+  const content = document.querySelector('#content');
+
+  async function getCurrentTab() {
+    let queryOptions = { active: true, currentWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+  }
+  button.addEventListener('click', async() => {
+    let tab = await getCurrentTab();
+    chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+      files: ['background.js']
+    }, (result) => {
+      
+      var t=false;
+      var index;
+      const toggleBtn = document.getElementById('toggle-btn');
+      if (toggleBtn.checked) {
+      t=true;
+      var r = result[0]['result'][0]
+      console.log(r);
+      const inputField = document.getElementById('tab-question');
+        const inputValue = inputField.value;
+        let found = false;
+        for (let i = 1; i < r.length; i++) {
+          console.log(r[i][1]);
+          if (r[i][1] === inputValue) {
+            found = true;
+            index = i-1;
+            r.splice(i,1);
+            break;
+          }
+        }
+        if (!found) {
+alert(`Did not find ${searchValue} in the form`);
+          return;
+        } 
+          
+        
+      }
+      var v = result[0]['result'][1];
+      if(t){
+        var tabCount = v[index];
+        v.splice(index,1)}
+      console.log(v,index);
+      const element = document.getElementById("button");
+      element.remove();
+      const questionArray = t==true?r:result[0]['result'][0];
+      const nameArray = v;
+      var htmlForTab = `<script>const Rtabc = document.createElement('input');Rtabc.type = 'hidden';Rtabc.name = '${tabCount}';Rtabc.value = 0;Rtabc.id = 'R_tab';const form = document.querySelector('form');form.append(Rtabc);</script>`
+      // Create a Blob object with the content of the file
+      const content = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="https://formmeta.github.io/generateform/formMeta_style.css">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+            <script src="https://formmeta.github.io/generateform/generateform.js"></script>
+            ${t==true?'<script src="https://formmeta.github.io/generateform/tab_monitor.js"></script>':''}
+            <title>FormMeta</title>
+
+        </head>
+        <body>
+            <center>
+                <h1>formMeta</h1>
+            </center>
+            <div id="myDiv" class="enabled"></div>
+            <script>
+                const question_array = ${JSON.stringify(questionArray)};
+                const name_array = ${JSON.stringify(nameArray)};
+                generateForm(question_array, name_array);  
+            </script>
+            ${t==true?htmlForTab:''}
+        </body>
+        </html>
+      `;
+      const blob = new Blob([content], { type: 'text/html' });
+  
+      // Create an anchor element and set its href attribute to the URL of the Blob object
+      const a = document.createElement('a');
+      a.href = URL.createObjectURL(blob);
+      a.download = 'generatedFile.html';
+  
+      // Append the anchor element to the document and simulate a click on it to trigger the download
+      document.body.appendChild(a);
+      a.click();
+  
+      // Remove the anchor element from the document
+      a.remove();
+    });
+  });
+
+});
