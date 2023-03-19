132bd29d7faf6a2a78ab566bf7d8c7c91b02db9f
raguggg <ragu19062002@gmail.com>
2023-03-19 13:19:47 +0530

    change generatefile to index

--- popup.js
+++ popup.js
@@ -144,7 +144,7 @@ document.addEventListener('DOMContentLoaded', () => {
       // Create an anchor element and set its href attribute to the URL of the Blob object
       const a = document.createElement('a');
       a.href = URL.createObjectURL(blob);
-      a.download = 'generatedFile.html';
+      a.download = 'index.html';
   
       // Append the anchor element to the document and simulate a click on it to trigger the download
       document.body.appendChild(a);
